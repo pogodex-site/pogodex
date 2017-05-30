275fd7c02f0dd481f4522178192145cbc1382589
freddec <deconinck.frederic@yahoo.fr>
2017-05-30 08:34:11 +0000

    dev

--- server/settings.py
+++ server/settings.py
@@ -86,7 +86,7 @@ USE_L10N = True
 USE_TZ = True
 
 import os
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
