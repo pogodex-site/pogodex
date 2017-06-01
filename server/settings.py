1ea28a8fbd6ec9b9f568f34bcd75ef92093f8547
freddec <deconinck.frederic@yahoo.fr>
2017-06-01 15:09:54 +0000

    dev

--- server/settings.py
+++ server/settings.py
@@ -92,6 +92,7 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
