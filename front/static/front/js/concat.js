fd1dfee10bb58bc7616e9ed491893c0f07cfc5fa
freddec <deconinck.frederic@yahoo.fr>
2017-06-08 14:25:10 +0000

    dev

--- front/static/front/js/concat.js
+++ front/static/front/js/concat.js
@@ -1909,11 +1909,11 @@ angular.module('AngularApp').controller('ProfileCtrl', function($scope, $locatio
 	
 	$scope.profile = ProfileService.getProfile();
 	
-	$scope.level_list = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-						 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-						 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-						 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-						];
+	$scope.profileLevelList = [  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+								11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+								21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+								31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+							  ];
 	
 	$scope.profileModel = {team: $scope.profile.team, level: $scope.profile.level};
 	
